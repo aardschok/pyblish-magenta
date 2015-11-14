44220da019a58608f055e92c1fd538ff2afdf6aa
Roy Nieterau <roy_nieterau@hotmail.com>
2015-11-14 09:51:24 +0100

    Raise error when scene is unsaved

--- pyblish_magenta/plugins/pipeline/cleanup_modifications.py
+++ pyblish_magenta/plugins/pipeline/cleanup_modifications.py
@@ -10,4 +10,12 @@ class CleanupModifications(pyblish.api.Plugin):
 
     def process(self, context):
         from maya import cmds
+
+        # If the scene wasn't saved we'll raise an error. This is more
+        # convenient because otherwise the save command will raise a Maya
+        # pop-up plus we can give a clearer error description.
+        scene_name = cmds.file(q=1, sn=True)
+        if not scene_name:
+            raise RuntimeError("Can't save modifications for an unsaved scene")
+
         cmds.file(save=True)
