aa2cb8c7aadd5d21675628bb15b1d08c6bacf2f5
Roy Nieterau <roy_nieterau@hotmail.com>
2016-02-20 23:49:10 +0100

    Collect comment as additional family for an instance if "notes" present

---
+++ pyblish_magenta/plugins/pipeline/collect_comment.py
@@ -0,0 +1,36 @@
+import pyblish.api
+from maya import cmds
+
+
+class CollectComment(pyblish.api.InstancePlugin):
+    """Collect comments for the instances in Maya.
+
+    This adds the `comment` family to any of the instances
+    when they have some content in the "notes" area
+
+    """
+    label = "Maya Collect Comment"
+
+    order = pyblish.api.CollectorOrder + 0.2
+    hosts = ["maya"]
+
+    def process(self, instance):
+
+        node_attr = "{0}.notes".format(instance.id)
+        if not cmds.objExists(node_attr):
+            return
+
+        comment = cmds.getAttr(node_attr)
+        if not comment:
+            return
+
+        # Add comment family
+        families = instance.data.get("families", [])
+        if "comment" not in families:
+            families.append("comment")
+            instance.data['families'] = families
+
+        # And add the data
+        instance.data['comment'] = comment
+
+        self.log.info("Collected comment: {0}".format(comment))
