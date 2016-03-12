a3fcce78c4fbdd77cedaed8670e03b84e6e00023
Roy Nieterau <roy_nieterau@hotmail.com>
2016-03-12 16:46:33 +0100

    Update model content

--- pyblish_magenta/plugins/workflow/maya/validate_model_content.py
+++ pyblish_magenta/plugins/workflow/maya/validate_model_content.py
@@ -3,6 +3,12 @@ import pyblish_magenta.api
 
 
 class ValidateModelContent(pyblish.api.InstancePlugin):
+    """Adheres to the content of 'model' family
+
+    - Must have one top group named: geo_GRP
+    - Must only contain: transforms, meshes and groups
+
+    """
 
     order = pyblish_magenta.api.ValidateContentsOrder
     families = ["model"]
@@ -11,12 +17,21 @@ class ValidateModelContent(pyblish.api.InstancePlugin):
 
     def process(self, instance):
         from maya import cmds
-        allowed = ('mesh', 'transform', 'nurbsCurve')
 
+        # Ensure only valid node types
+        allowed = ('mesh', 'transform', 'nurbsCurve')
         nodes = cmds.ls(instance, long=True)
         valid = cmds.ls(instance, long=True, type=allowed)
-
-        [self.log.info("Considering %s.." % node) for node in nodes]
         invalid = set(nodes) - set(valid)
 
         assert not invalid, "These nodes are not allowed: %s" % invalid
+
+        # Top group
+        assemblies = cmds.ls(instance, assemblies=True, long=True)
+
+        if len(assemblies) != 1:
+            raise RuntimeError("Model must have one top group")
+
+        if assemblies[0] != "|geo_GRP":
+            raise RuntimeError("Top group must be named 'geo_GRP'")
+
