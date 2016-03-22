240c6b28018ce704cde026ad1cc0919e97818d51
Roy Nieterau <roy_nieterau@hotmail.com>
2016-03-22 13:55:48 +0100

    Pylinter validate_references_no_failed_edits

--- pyblish_magenta/plugins/workflow/maya/validate_references_no_failed_edits.py
+++ pyblish_magenta/plugins/workflow/maya/validate_references_no_failed_edits.py
@@ -4,17 +4,14 @@ from maya import cmds
 
 
 class ValidateReferencesNoFailedEdits(pyblish.api.InstancePlugin):
-    """Validate that all referenced nodes' reference nodes don't have failed reference edits.
+    """Validate that all referenced nodes' reference nodes don't have failed
+    reference edits.
 
-        Failed reference edits can happen if you apply a change to a referenced object in the scene and then change
-        the source of the reference (that actual file) to remove the object. The reference edit can't be applied to
-        the object because it doesn't exist anymore, hence a 'failed reference edit'.
-        It's a thing most artists won't check but in large sets this could bloat file sizes unnecessarily.
-
-        .. note::
-            The terminology here can easily confuse you.
-            reference node: The node that is the actual reference containing the nodes (type: referenceNode)
-            referenced nodes: The nodes contained within the reference (type: any type of nodes)
+    Failed reference edits can happen if you apply a change to a referenced
+    object in the scene and then change the source of the reference
+    (referenced file) to remove the object. The reference edit can't be
+    applied to the node because it is missing, hence a "failed edit". This
+    could unnecessarily bloat file sizes and degrade load/save speed.
 
     """
 
@@ -27,13 +24,21 @@ class ValidateReferencesNoFailedEdits(pyblish.api.InstancePlugin):
     label = 'References Failed Edits'
 
     def process(self, instance):
-        """Process all the nodes in the instance"""
+        """Process all the nodes in the instance
+
+        Terminology:
+            reference node: The node that is the actual reference containing
+                the nodes (type: referenceNode)
+            referenced nodes: The nodes contained within the reference
+                (type: any type of nodes)
+        """
 
         referenced_nodes = cmds.ls(instance, referencedNodes=True, long=True)
         if not referenced_nodes:
             return
 
-        # Get reference nodes from referenced nodes (note that reference_nodes != referenced_nodes)
+        # Get reference nodes from referenced nodes
+        # (note that reference_nodes != referenced_nodes)
         reference_nodes = set()
         for node in referenced_nodes:
             reference_node = cmds.referenceQuery(node, referenceNode=True)
@@ -43,10 +48,13 @@ class ValidateReferencesNoFailedEdits(pyblish.api.InstancePlugin):
         # Check for failed edits on each reference node.
         invalid = []
         for reference_node in reference_nodes:
-            failed_edits = cmds.referenceQuery(reference_node, editNodes=True, failedEdits=True, successfulEdits=True)
+            failed_edits = cmds.referenceQuery(reference_node,
+                                               editNodes=True,
+                                               failedEdits=True,
+                                               successfulEdits=False)
             if failed_edits:
                 invalid.append(reference_node)
 
         if invalid:
-            raise ValueError("Reference nodes found with failed reference edits: {0}".format(invalid))
-
+            raise ValueError("Reference nodes found with failed "
+                             "reference edits: {0}".format(invalid))
