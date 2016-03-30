135b4af7d6d8edc6c9b508465cca5b13d57e02f3
Roy Nieterau <roy_nieterau@hotmail.com>
2016-03-30 07:29:15 +0200

    Add SelectInvalidAction

---
+++ pyblish_magenta/action.py
@@ -0,0 +1,42 @@
+
+import pyblish.api
+
+
+class SelectInvalidAction(pyblish.api.Action):
+    """Select invalid nodes in Maya when plug-in failed.
+
+    To retrieve the invalid nodes this assumes a static `get_invalid()`
+    method is available on the plugin.
+
+    """
+    label = "Select invalid"
+    on = "failed"  # This action is only available on a failed plug-in
+    icon = "search"  # Icon from Awesome Icon
+
+    def process(self, context, plugin):
+        from maya import cmds
+
+        # Get the errored instances
+        self.log.info("Finding failed instances..")
+        errored_instances = []
+        for result in context.data["results"]:
+            if result["error"]:
+                instance = result["instance"]
+                errored_instances.append(instance)
+
+        # Apply pyblish.logic to get the instances for the plug-in
+        instances = pyblish.api.instances_by_plugin(errored_instances, plugin)
+
+        # Get the invalid nodes for the plug-ins
+        self.log.info("Finding invalid nodes..")
+        invalid = list()
+        for instance in instances:
+            invalid_nodes = plugin.get_invalid(instance)
+            invalid.extend(invalid_nodes)
+
+        if invalid:
+            self.log.info("Selecting invalid nodes: %s" % ", ".join(invalid))
+            cmds.select(invalid, r=1, noExpand=True)
+        else:
+            self.log.info("No invalid nodes found.")
+            cmds.select(deselect=True)
