8771bce6b052f447fbae34066f6304a0c2686e0d
Roy Nieterau <roy_nieterau@hotmail.com>
2016-03-30 07:43:57 +0200

    Add select invalid action

--- pyblish_magenta/plugins/workflow/maya/validate_mesh_non_manifold.py
+++ pyblish_magenta/plugins/workflow/maya/validate_mesh_non_manifold.py
@@ -1,6 +1,7 @@
 import pyblish.api
 import pyblish_magenta.api
-from maya import cmds
+
+from pyblish_magenta.action import SelectInvalidAction
 
 
 class ValidateMeshNonManifold(pyblish.api.InstancePlugin):
@@ -12,15 +13,26 @@ class ValidateMeshNonManifold(pyblish.api.InstancePlugin):
     category = 'geometry'
     version = (0, 1, 0)
     label = 'Mesh Non-Manifold Vertices/Edges'
+    actions = [SelectInvalidAction]
 
-    def process(self, instance):
-        """Process all the nodes in the instance 'objectSet'"""
+    @staticmethod
+    def get_invalid(instance):
+        from maya import cmds
+
         meshes = cmds.ls(instance, type='mesh', long=True)
 
         invalid = []
         for mesh in meshes:
-            if cmds.polyInfo(mesh, nonManifoldVertices=True) or cmds.polyInfo(mesh, nonManifoldEdges=True):
+            if (cmds.polyInfo(mesh, nonManifoldVertices=True) or
+                    cmds.polyInfo(mesh, nonManifoldEdges=True)):
                 invalid.append(mesh)
+
+        return invalid
+
+    def process(self, instance):
+        """Process all the nodes in the instance 'objectSet'"""
+
+        invalid = self.get_invalid(instance)
 
         if invalid:
             raise ValueError("Meshes found with non-manifold "
