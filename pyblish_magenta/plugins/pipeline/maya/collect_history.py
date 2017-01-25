7021eca4b71dc5ec5fe0582ea32c05c4bfcbfb52
Roy Nieterau <roy_nieterau@hotmail.com>
2017-01-25 14:42:17 +0100

    Avoid collecting render layers in the history (in rigs)

--- pyblish_magenta/plugins/pipeline/maya/collect_history.py
+++ pyblish_magenta/plugins/pipeline/maya/collect_history.py
@@ -4,6 +4,9 @@ import pyblish.api
 class CollectMayaHistory(pyblish.api.InstancePlugin):
     """Collect history for instances from the Maya scene
 
+    Note:
+        This removes render layers collected in the history
+
     This is separate from Collect Instances so we can target it towards only
     specific family types.
 
@@ -22,6 +25,12 @@ class CollectMayaHistory(pyblish.api.InstancePlugin):
         history = cmds.listHistory(instance,
                                    leaf=False) or []
         history = cmds.ls(history, long=True)
+
+        # Remove invalid node types (like renderlayers)
+        invalid = cmds.ls(history, type="renderLayer", long=True)
+        if invalid:
+            invalid = set(invalid)  # optimize lookup
+            history = [x for x in history if x not in invalid]
 
         # Combine members with history
         members = instance[:] + history
